e3c9a0a0def494c5756be7d69fa125348c858161
Simran shrestha <shresthasimran62@gmail.com>
2022-03-04 18:44:09 +0545

    incomplete form

--- lib/widgets/signup.dart
+++ lib/widgets/signup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
-import 'package:health_management_app/ui/top_bar.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignUp extends StatefulWidget {
@@ -11,215 +11,147 @@ class SignUp extends StatefulWidget {
   _SignUpState createState() => _SignUpState();
 }
 
+List<DropdownMenuItem<String>> get dropdownItems {
+  List<DropdownMenuItem<String>> menuItems = [
+    DropdownMenuItem(child: Text("Doctor"), value: "Doctor"),
+    DropdownMenuItem(child: Text("Nurse"), value: "Nurse"),
+    DropdownMenuItem(child: Text("Health Worker"), value: "Health Worker"),
+    DropdownMenuItem(child: Text("Compounder"), value: "Compounder"),
+    DropdownMenuItem(child: Text("Pharmacist"), value: "Pharmacist"),
+  ];
+  return menuItems;
+}
+
 class _SignUpState extends State<SignUp> {
+  String? selectedValue = null;
+  final _dropdownFormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned.fill(
-              child: Image(
-                image: AssetImage('images/bg.png'),
-                fit: BoxFit.cover,
-                color: Colors.white.withOpacity(0.5),
-                colorBlendMode: BlendMode.modulate,
-              ),
+    final deviceSize = MediaQuery.of(context).size;
+    return Stack(alignment: Alignment.center, children: [
+      Container(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            margin: EdgeInsets.only(top: 10.5),
+            padding: const EdgeInsets.all(0.0),
+            width: deviceSize.width * 0.85,
+            height: deviceSize.height * 0.60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(30)),
             ),
-            TopBar(),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: (Column(
                 children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 12.5),
-                    padding: const EdgeInsets.all(0.0),
-                    height: 480,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
                     child: Container(
-                      margin: EdgeInsets.all(25),
-                      child: (Column(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(80.0),
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: ToggleSwitch(
+                          minHeight: 35,
+                          minWidth: MediaQuery.of(context).size.width * 0.3,
+                          cornerRadius: 80.0,
+                          activeBgColor: [Color(0xff5A227E)],
+                          activeFgColor: Colors.white,
+                          inactiveBgColor: Colors.white,
+                          inactiveFgColor: Colors.black,
+                          initialLabelIndex: 1,
+                          totalSwitches: 2,
+                          radiusStyle: true,
+                          labels: const ['Log In', 'Sign Up'],
+                          onToggle: (index) {
+                            widget.toggleView();
+                          }),
+                    ),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: 'Full Name',
+                    ),
+                  ),
+                  Form(
+                    key: _dropdownFormKey,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 20),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(80.0),
-                                border: Border.all(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              child: ToggleSwitch(
-                                  minHeight: 35,
-                                  minWidth:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  cornerRadius: 80.0,
-                                  activeBgColor: [Color(0xff5A227E)],
-                                  activeFgColor: Colors.white,
-                                  inactiveBgColor: Colors.white,
-                                  inactiveFgColor: Colors.black,
-                                  initialLabelIndex: 1,
-                                  totalSwitches: 2,
-                                  radiusStyle: true,
-                                  labels: const ['Log In', 'Sign Up'],
-                                  onToggle: (index) {
-                                    widget.toggleView();
-                                  }),
+                          DropdownButtonFormField(
+                              validator: (value) =>
+                                  value == null ? "Select an item" : null,
+                              value: selectedValue,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedValue = newValue!;
+                                });
+                              },
+                              items: dropdownItems),
+                        ]),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      labelText: 'Speciality',
+                      fillColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      suffixIcon: Icon(
+                        FontAwesomeIcons.eye,
+                        size: 15,
+                      ),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Confirm Password",
+                      suffixIcon: Icon(
+                        FontAwesomeIcons.eye,
+                        size: 15,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: SizedBox(
+                      width: double.infinity, // match_parent
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Theme.of(context).primaryColor),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
                             ),
                           ),
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              hintText: 'Full Name',
-                            ),
-                          ),
-                          // Form(
-                          //   key: formKey,
-                          //   autovalidateMode:
-                          //       AutovalidateMode.disabled, // <- here
-
-                          // child: Column(
-                          //   mainAxisAlignment: MainAxisAlignment.start,
-                          //   children: <Widget>[
-                          //     Container(
-                          //       padding: EdgeInsets.all(16),
-                          //       child: DropDownFormField(
-                          //         titleText: 'My workout',
-                          //         hintText: 'Please choose one',
-                          //         value: _myActivity,
-                          //         onSaved: (value) {
-                          //           setState(() {
-                          //             _myActivity = value;
-                          //           });
-                          //         },
-                          //         onChanged: (value) {
-                          //           setState(() {
-                          //             _myActivity = value;
-                          //           });
-                          //         },
-                          //         dataSource: [
-                          //           {
-                          //             "display": "Running",
-                          //             "value": "Running",
-                          //           },
-                          //           {
-                          //             "display": "Climbing",
-                          //             "value": "Climbing",
-                          //           },
-                          //           {
-                          //             "display": "Walking",
-                          //             "value": "Walking",
-                          //           },
-                          //           {
-                          //             "display": "Swimming",
-                          //             "value": "Swimming",
-                          //           },
-                          //           {
-                          //             "display": "Soccer Practice",
-                          //             "value": "Soccer Practice",
-                          //           },
-                          //           {
-                          //             "display": "Baseball Practice",
-                          //             "value": "Baseball Practice",
-                          //           },
-                          //           {
-                          //             "display": "Football Practice",
-                          //             "value": "Football Practice",
-                          //           },
-                          //         ],
-                          //         textField: 'display',
-                          //         valueField: 'value',
-                          //       ),
-                          //     ),
-                          //   ],
-                          //   ),
-                          // ),
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              hintText: 'Full Name',
-                            ),
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              hintText: 'Full Name',
-                            ),
-                          ),
-
-                          // Row(
-                          //   children: [
-                          //     TextField(
-                          //       keyboardType: TextInputType.text,
-                          //       decoration: InputDecoration(
-                          //         hintText: 'Email',
-                          //       ),
-                          //     ),
-                          //     TextField(
-                          //       keyboardType: TextInputType.text,
-                          //       decoration: InputDecoration(
-                          //         hintText: 'Mobile Number',
-                          //       ),
-                          //     ),
-                          //   ],
-                          // ),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                              suffixIcon: Icon(
-                                FontAwesomeIcons.eye,
-                                size: 15,
-                              ),
-                            ),
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Confirm Password",
-                              suffixIcon: Icon(
-                                FontAwesomeIcons.eye,
-                                size: 15,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 15.0),
-                            child: SizedBox(
-                              width: double.infinity, // match_parent
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                child: const Text(
-                                  'Sign Up',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Theme.of(context).primaryColor),
-                                  shape:
-                                      MaterialStateProperty.all<OutlinedBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )),
+                        ),
+                      ),
                     ),
                   ),
                 ],
-              ),
+              )),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
-    );
+    ]);
   }
 }
