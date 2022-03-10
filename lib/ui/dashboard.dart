7bc15d64f6fd127f185b2cc324950060180ed39b
Simran shrestha <shresthasimran62@gmail.com>
2022-03-10 23:21:10 +0545

    incomplete dashboard screen

---
+++ lib/ui/dashboard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({Key? key, required this.accessToken}) : super(key: key);
+  final String accessToken;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(accessToken),
+    );
+  }
+}
