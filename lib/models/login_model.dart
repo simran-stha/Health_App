83a0be4cbe4f675472323463dde8b0ba604f3cb3
Simran shrestha <shresthasimran62@gmail.com>
2022-03-13 16:43:10 +0545

    login model updated

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,47 +1,16 @@
 import 'dart:convert';
 
-LoginModel loginModelFromJson(String str) =>
-    LoginModel.fromJson(json.decode(str));
-
-String loginModelToJson(LoginModel data) => json.encode(data.toJson());
-
-class LoginModel {
-  LoginModel({
-     this.email,
-     this.deviceId,
-     this.password,
-  });
-
-  String? email;
-  String? deviceId;
-  String? password;
-
-  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
-        email: json["email"],
-        deviceId: json["device_id"],
-        password: json["password"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "email": email,
-        "device_id": deviceId,
-        "password": password,
-      };
-}
-
 LoginResponseModel loginResponseModelFromJson(String str) =>
     LoginResponseModel.fromJson(json.decode(str));
 
-String loginResponseModelToJson(LoginResponseModel data) =>
-    json.encode(data.toJson());
-
 class LoginResponseModel {
   LoginResponseModel({
-     this.token,
-     this.user,
-     this.email,
-     this.contactNumber,
-     this.isNumberVerified,
+    this.token,
+    this.user,
+    this.email,
+    this.contactNumber,
+    this.isNumberVerified,
+    this.msg,
   });
 
   String? token;
@@ -49,6 +18,7 @@ class LoginResponseModel {
   String? email;
   String? contactNumber;
   bool? isNumberVerified;
+  String? msg;
 
   factory LoginResponseModel.fromJson(Map<String, dynamic> json) =>
       LoginResponseModel(
@@ -57,13 +27,40 @@ class LoginResponseModel {
         email: json["email"],
         contactNumber: json["contactNumber"],
         isNumberVerified: json["isNumberVerified"],
+        msg: json["msg"],
+      );
+}
+
+LoginRequestModel loginRequestModelFromJson(String str) =>
+    LoginRequestModel.fromJson(json.decode(str));
+
+String loginRequestModelToJson(LoginRequestModel data) =>
+    json.encode(data.toJson());
+
+class LoginRequestModel {
+  LoginRequestModel({
+    this.email,
+    this.deviceId = "12345",
+    this.password,
+  });
+
+  String? email;
+  String? deviceId;
+  String? password;
+
+  factory LoginRequestModel.fromJson(Map<String, dynamic> json) =>
+      LoginRequestModel(
+        email: json["email"],
+        deviceId: json["device_id"],
+        password: json["password"],
       );
 
-  Map<String, dynamic> toJson() => {
-        "token": token,
-        "user": user,
-        "email": email,
-        "contactNumber": contactNumber,
-        "isNumberVerified": isNumberVerified,
-      };
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {
+      "email": email,
+      "device_id": deviceId,
+      "password": password,
+    };
+    return map;
+  }
 }
